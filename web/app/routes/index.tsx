f66c2de19e56d13c6fa4853f25591a3523382eae
xyy <xyycove@gmail.com>
2025-12-03 11:37:13 +0800

    feat: 调整界面

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       {Array.from({ length: 100 }, (_, index) => (
-        <div>12312</div>
+        <div key={index}>12312</div>
       ))}
     </div>
   );
